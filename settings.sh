b29a9aaefe90cb072e149f0440db395ed346a194
Curtis Alexander <c4lex@yahoo.com>
2016-03-20 12:31:49 -0500

    add URLs and vars for R and RStudio

--- settings.sh
+++ settings.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 
 ###################
-# global settings #
+# system settings #
 ###################
 export SYSTEM_LABEL="flyingfish"
 export SYSTEM_NAME="flyingfish"
+
+############
+# app URLs #
+############
+export R_URL="https://cran.rstudio.com/bin/macosx/R-3.2.4.pkg"
+export RSTUDIO_URL="https://download1.rstudio.org/RStudio-0.99.893.dmg"
+# RSTUDIO_MNT_NAME should mirror the dmg file name
+export RSTUDIO_MNT_NAME="RStudio-0.99.893"
+export MINICONDA_URL="https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh"
+
+############
+# dotfiles #
+############
 #export DOTFILES_DIR
