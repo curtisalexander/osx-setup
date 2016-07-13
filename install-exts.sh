cc34e3966803ba2bc7915b347eae75aff80d2ae1
Curtis Alexander <calex@calex.org>
2016-07-12 21:48:43 -0500

    fix merge conflicts

--- install-exts.sh
+++ install-exts.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/env bash
 
 #######
@@ -8,29 +7,3 @@
 # vim package manager
 
 # vimrc
-||||||| merged common ancestors
-=======
-#!/usr/bin/env bash
-
-##########
-# prezto #
-##########
-# utilize a heredoc as must be executed using zsh
-#/bin/zsh <<EOF
-#git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-#setopt EXTENDED_GLOB
-#for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-#  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-#done
-#EOF
-
-#######
-# vim #
-#######
-
-# vundle
-
-# vundle packages
-
-# vimrc
->>>>>>> 4a76d782c33d571a34bc111e88cfb3bea3775684
